1a9e7b06056ee42e09d5f15c8733bd92f5b2b957
RedRangerShiba <158241086+RedRangerShiba@users.noreply.github.com>
2024-01-30 21:46:22 -0500

    Add files via upload

---
+++ Lab2/Stars.java
@@ -0,0 +1,63 @@
+//Jayden Hood
+//CSCI 161
+//2 7 2024
+
+public class Stars {
+
+	public static void main(String[] args) {
+		drawLine(); 
+		drawBottom(); 	
+		drawLine(); 
+		drawBottom(); 
+		drawLine(); 
+		drawTop(); 
+		drawLine(); 
+		drawBottom(); 
+	}
+	
+	//draws the two lines
+	public static void drawLine() {
+		System.out.println("*****"); 
+		System.out.println("*****"); 
+	}
+	//draws the x
+	public static void drawBottom() {
+		System.out.println(" * *"); 
+		System.out.println("  *");
+		System.out.println(" * *"); 
+	}
+    //draws the two lines
+	public static void drawLine() {
+		System.out.println("*****"); 
+		System.out.println("*****"); 
+	}
+	//draws the x
+	public static void drawBottom() {
+		System.out.println(" * *"); 
+		System.out.println("  *");
+		System.out.println(" * *"); 
+	}
+	//draws the two lines
+	public static void drawLine() {
+		System.out.println("*****"); 
+		System.out.println("*****"); 
+	}
+	//draws the bar
+	public static void drawTop() {
+		System.out.println("  *");
+		System.out.println("  *");
+		System.out.println("  *");
+	}
+	//draws the two lines
+	public static void drawLine() {
+		System.out.println("*****"); 
+		System.out.println("*****"); 
+	}
+	//draws the x
+	public static void drawBottom() {
+		System.out.println(" * *"); 
+		System.out.println("  *");
+		System.out.println(" * *"); 
+	}
+	
+} 
